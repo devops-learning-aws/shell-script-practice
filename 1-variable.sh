76cc9500a59d09f31b9e110daf97f17fc6e8ed53
harikrishnaaswini <annametiaswini.1999@gmail.com>
2025-07-28 11:15:12 +0530

    upated the  code

--- 1-variable.sh
+++ 1-variable.sh
@@ -3,5 +3,5 @@
 PERSON1 ="RAMU"
 PERSON2="RAHUL"
 
-echo "$PERSON1:HELLO $PERSON2,How are you"
-echo "$PERSON2:Hi $PERSON1,I am fine"
+echo "$PERSON1 ::HELLO $PERSON2,How are you"
+echo "$PERSON2 ::Hi $PERSON1,I am fine"
